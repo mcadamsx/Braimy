0b3cf8439bc9ff7ce0125266ea4cb5351f381fa0
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-03-08 12:48:51 +0000

    courses template implementation

---
+++ src/app/components/reading/reading.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading',
+  standalone: true,
+  imports: [],
+  templateUrl: './reading.component.html',
+  styleUrl: './reading.component.css'
+})
+export class ReadingComponent {
+  category = "category";
+  courseName = "course Name"
+
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import {SidebarComponent} from "../../components/sidebar/sidebar.component";
+import {CourseComponent} from "../../components/course/course.component";
+import {ReadingComponent} from "../../components/reading/reading.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, CourseComponent, ReadingComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  name= "user";
-
+  name = 'user';
 }
